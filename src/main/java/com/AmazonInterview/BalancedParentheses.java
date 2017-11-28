0c23264d7b90087ee267e825c71edd8a0b769ddc
pjai60 <palash340@gmail.com>
2017-11-28 21:15:38 +0530

    More code

--- src/main/java/com/AmazonInterview/BalancedParentheses.java
+++ src/main/java/com/AmazonInterview/BalancedParentheses.java
@@ -1,7 +1,39 @@
 package com.AmazonInterview;
 
+import java.util.Stack;
+
 /**
  * Created by pjai60 on 11/28/2017.
  */
 public class BalancedParentheses {
+    public static void main(String[] args) {
+        BalancedParentheses bp = new BalancedParentheses();
+        System.out.println(bp.isBalanced("((){})".toCharArray()));
+    }
+
+    public boolean isBalanced(char[] arr){
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] == '{' || arr[i] == '(' || arr[i] == '[')
+                stack.push(arr[i]);
+            if(arr[i] == '}' || arr[i] == ')' || arr[i] == ']'){
+                if(stack.isEmpty())
+                    return false;
+                if(!isMatchingPair(stack.pop(), arr[i]))
+                    return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+
+    public boolean isMatchingPair(char opening, char closing){
+        if(opening == '(' && closing == ')')
+            return true;
+        if(opening == '{' && closing == '}')
+            return true;
+        if(opening == '[' && closing == ']')
+            return true;
+        return false;
+    }
 }
