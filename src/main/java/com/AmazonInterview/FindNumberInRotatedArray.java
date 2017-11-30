b177f204fc2a543df5ebf66b740f6665041168c1
pjai60 <palash340@gmail.com>
2017-11-30 11:01:36 +0530

    Dynamic Programming Examples

--- src/main/java/com/AmazonInterview/FindNumberInRotatedArray.java
+++ src/main/java/com/AmazonInterview/FindNumberInRotatedArray.java
@@ -12,31 +12,27 @@ public class FindNumberInRotatedArray {
     public static void main(String[] args) {
         FindNumberInRotatedArray findNumberInRotatedArray = new FindNumberInRotatedArray();
         int[] arr =  {17, 21, 2, 3, 6, 11};
-        int index = findNumberInRotatedArray.findValue(arr, 3, 0, arr.length - 1);
+        int index = findNumberInRotatedArray.findValue(arr, 11, 0, arr.length - 1);
         if(index == -1)
             System.out.println("Element not found");
         else
             System.out.println("Element found at Index " + index);
     }
 
-    public int findValue(int[] arr, int valueToFind, int start, int finish){
-        if(arr.length < 0)
+    public int findValue(int[] arr, int valueToFind, int start, int finish) {
+        if (arr.length < 0)
             return -1;
-        if(start == finish && valueToFind == arr[start])
+        if (start == finish && valueToFind == arr[start])
             return start;
-        if(finish > start){
-            if(arr[start] > arr[finish]){
-                int mid = (start + finish) / 2;
-                if(arr[mid] <= arr[finish]){
-                    if(valueToFind >= arr[mid] && valueToFind <= arr[finish]) {
-                        System.out.println(" if mid = " + mid + ", start =" + start + ", finish = " + finish);
-                        return findValue(arr, valueToFind, mid, finish);
-                    }
-                }else{
-                    System.out.println(" else mid = " + mid);
-                    if(valueToFind <= arr[start] && valueToFind >= arr[start])
-                        return findValue(arr, valueToFind, start, mid);
-                }
+        if (finish > start) {
+            int mid = (start + finish) / 2;
+            if (valueToFind == arr[mid])
+                return mid;
+            else {
+                if(arr[mid] < arr[finish] && valueToFind >= arr[mid] && valueToFind <= arr[finish])
+                    return findValue(arr, valueToFind,mid+1, finish);
+                else
+                    return findValue(arr, valueToFind, start,mid-1);
             }
         }
         return -1;
