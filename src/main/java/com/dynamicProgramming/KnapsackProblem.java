05ec70dbbd9faef6519e90bf1215d89ecf01ea14
Palash Jain <palash340@gmail.com>
2017-12-03 17:44:15 +0530

    Recursion Programming

--- src/main/java/com/dynamicProgramming/KnapsackProblem.java
+++ src/main/java/com/dynamicProgramming/KnapsackProblem.java
@@ -10,11 +10,12 @@ public class KnapsackProblem {
         KnapsackProblem kp = new KnapsackProblem();
         int[] valArr = {1,4,5,7};
         int[] weightArr = {1,3,4,5};
-        kp.buildMatrix(weightArr, valArr, 7);
+        System.out.println(kp.buildMatrix(weightArr, valArr, 7));
+        System.out.println(kp.knapsackWithRec(weightArr,valArr,7,0));
 
     }
 
-    public void buildMatrix(int[] weightArr, int[] valArr, int maxWeight){
+    public int buildMatrix(int[] weightArr, int[] valArr, int maxWeight){
         int[][] matrix = new int[valArr.length][maxWeight+1];
         //initialize zeroth column
         for(int i = 0; i < valArr.length ; i++)
@@ -32,9 +33,35 @@ public class KnapsackProblem {
             }
             row++;
         }
-        System.out.println(Arrays.deepToString(matrix));
-        System.out.println(matrix.toString());
+        //System.out.println(Arrays.deepToString(matrix));
+        return findMax(matrix);
     }
 
+
+    //TODO code is not working correctly
+    public int knapsackWithRec(int[] weight, int[] val, int w, int itemNum){
+        if(w == 0 || itemNum == weight.length)
+            return 0;
+        // Item weight is greater than expected (don't pick item)
+        if(weight[itemNum] > w)
+            knapsackWithRec(weight,val,w,itemNum+1);
+        // Pick the item
+        int rMax = val[itemNum] + knapsackWithRec(weight, val, w - weight[itemNum], itemNum+1);
+        // Don't pick item
+        int lMax = knapsackWithRec(weight,val, w, itemNum+1);
+
+        return Math.max(rMax, lMax);
+    }
+
+    public int findMax(int[][] result){
+        int max = Integer.MIN_VALUE +1;
+        for (int i = 0; i < result.length ; i++) {
+            for (int j = 0; j < result[i].length; j++) {
+                if(max < result[i][j])
+                    max = result[i][j];
+            }
+        }
+        return max;
+    }
 
 }
