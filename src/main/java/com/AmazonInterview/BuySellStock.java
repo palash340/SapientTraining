7e2b5a69df8482117e7045ac14c02e213b0add42
Palash Jain <palash340@gmail.com>
2017-12-03 21:45:34 +0530

    Recursion Programming

---
+++ src/main/java/com/AmazonInterview/BuySellStock.java
@@ -0,0 +1,34 @@
+package com.AmazonInterview;
+
+public class BuySellStock {
+    static class Trade{
+        int buyDay;
+        int sellDay;
+        int buyPrice;
+        int sellPrice;
+    }
+
+    // Driver method
+    public static void main(String[] args) {
+
+    }
+
+
+    public Trade[] findMaximumProfit(int[] prices){
+        Trade[] trades = new Trade[prices.length / 2 + 1];
+        int count = 0;
+        while(count == prices.length){
+
+
+            // Local minima
+
+            //Local maxima
+
+        }
+
+
+        return trades;
+    }
+
+
+}
